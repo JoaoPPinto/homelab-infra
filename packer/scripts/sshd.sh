4b58b8f1ee12bdcb85d088f567d9bf0bde6d87c8
Joao Pinto <pinto@barcodeu.com>
2023-02-13 21:57:31 +0000

    packer: remove ubuntu template

--- packer/scripts/sshd.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-
-cat <<'EOF' > /etc/ssh/banner
-banner
-EOF
-
-#sed -i -e 's/#Banner none/Banner /etc/ssh/banner/g' /etc/ssh/sshd_config
-#sed -i 's/PermitRootLogin yes/PermitRootLogin no/g' /etc/ssh/sshd_config
