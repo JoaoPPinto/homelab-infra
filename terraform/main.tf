829d4a3cb43de9da60c97304ca971f3a9b80c072
Joao Pinto <pinto@fatska.xyz>
2023-03-07 19:04:51 +0000

    terraform: add dashboard vm

--- terraform/main.tf
+++ terraform/main.tf
@@ -25,3 +25,31 @@ module "budget_vm" {
     pihole.backup = pihole.zero
   }
 }
+
+module "dashboard_vm" {
+  source = "./modules/homelab_vm"
+  vm_name = "dashboard"
+  domain = var.base_domain
+  target_node = var.default_pve_node
+  vm_template = "debian-11-template"
+  memory = 512
+  disk_config = [ {
+    size = "5G"
+    storage = "local-lvm"
+    type = "scsi"
+  } ]
+  network_config = [ {
+    bridge = "vmbr0"
+    model = "virtio"
+  } ]
+  ipconfig = {
+    ip = "192.168.1.30"
+    gateway = "192.168.1.1"
+    cidr = "24"
+  }
+
+  providers = {
+    pihole.main = pihole
+    pihole.backup = pihole.zero
+  }
+}
--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,17 +1,20 @@
 output "vms" {
   value = [
-    module.budget_vm.vm_name
+    module.budget_vm.vm_name,
+    module.dashboard_vm.vm_name
   ]
 }
 
 output "vm_dns" {
   value = {
-    (module.budget_vm.vm_name) = module.budget_vm.dns
+    (module.budget_vm.vm_name) = module.budget_vm.dns,
+    (module.dashboard_vm.vm_name) = module.dashboard_vm.dns
   }
 }
 
 output "vm_ips" {
   value = {
-    (module.budget_vm.vm_name) = module.budget_vm.vm_ip
+    (module.budget_vm.vm_name) = module.budget_vm.vm_ip,
+    (module.dashboard_vm.vm_name) = module.dashboard_vm.vm_ip
   }
 }
